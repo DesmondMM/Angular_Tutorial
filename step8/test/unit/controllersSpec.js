33b6ebc9f2d64c3835ac5f5f5f5ad9bfa50db50a
DesmondMM <mutsakaradesmond@gmail.com>
2016-01-04 21:02:19 +0200

    Step 8 - Finished Tests

--- step8/test/unit/controllersSpec.js
+++ step8/test/unit/controllersSpec.js
@@ -1,5 +1,7 @@
 'use strict';
 
+beforeEach(module('phonecatApp'));
+
 /* jasmine specs for controllers go here */
 describe('PhoneCat controllers', function() {
 
@@ -32,6 +34,24 @@ describe('PhoneCat controllers', function() {
   });
 
 
-  describe('PhoneDetailCtrl', function(){
+ describe('PhoneDetailCtrl', function(){
+    var scope, $httpBackend, ctrl;
+
+    beforeEach(inject(function(_$httpBackend_, $rootScope, $routeParams, $controller) {
+      $httpBackend = _$httpBackend_;
+      $httpBackend.expectGET('phones/xyz.json').respond({name:'phone xyz'});
+
+      $routeParams.phoneId = 'xyz';
+      scope = $rootScope.$new();
+      ctrl = $controller('PhoneDetailCtrl', {$scope: scope});
+    }));
+
+
+    it('should fetch phone detail', function() {
+      expect(scope.phone).toBeUndefined();
+      $httpBackend.flush();
+
+      expect(scope.phone).toEqual({name:'phone xyz'});
+    });
   });
 });
